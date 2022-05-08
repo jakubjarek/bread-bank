7e115404c3eee4e823c65e1a08829ddad4cfe0c4
jakubjarek <71976031+jakubjarek@users.noreply.github.com>
2022-05-08 16:55:20 +0200

    Refactor History component.

--- src/views/History/History.tsx
+++ src/views/History/History.tsx
@@ -1,46 +1,57 @@
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
-import TransactionGroup from './TransactionGroup/TransactionGroup';
-
-import { nanoid } from 'nanoid';
+import { fromUnixTime, format } from 'date-fns';
 
 import { TransactionType } from './Transaction/Transaction';
+import useDoc from 'useDoc';
 
-export const data: TransactionType[] = [
-  {
-    amount: 23.5,
-    receiver: 'Jane Doe',
-    sender: 'John Doe',
-    received: true,
-    id: nanoid(),
-  },
-  {
-    amount: 23.5,
-    receiver: 'Jane Doe',
-    sender: 'John Doe',
-    received: false,
-    id: nanoid(),
-  },
-  {
-    amount: 23.5,
-    receiver: 'Jane Doe',
-    sender: 'John Doe',
-    received: true,
-    id: nanoid(),
-  },
-];
+import Spinner from 'shared/components/Spinner';
+import TransactionGroup from './TransactionGroup/TransactionGroup';
 
 const History = () => {
+  const [transactionList, setTransactionList] = useState<TransactionType[]>();
+  const [data, loading, error] = useDoc('transactions');
+
+  useEffect(() => {
+    if (loading || error) return;
+
+    if (data) {
+      const transactions = Object.values(data);
+      setTransactionList(transactions);
+    }
+  }, [loading, data, error]);
+
+  if (loading) {
+    return (
+      <LoadingContainer>
+        <Spinner />
+      </LoadingContainer>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        Something went wrong... 😕
+        <br />
+        {error}
+      </div>
+    );
+  }
+
   return (
     <GroupsContainer>
-      {/*  TransactionGroups here */}
-      {data.map((t) => {
-        return <TransactionGroup key={nanoid()} date={''} transactions={data} />;
-      })}
+      {transactionList &&
+        groupTransactionsByDate(transactionList).map(({ date, transactions }, idx) => (
+          <TransactionGroup key={idx} date={date} transactions={transactions} />
+        ))}
       <Outlet />
     </GroupsContainer>
   );
 };
+
+export default History;
 
 const GroupsContainer = styled.div`
   display: flex;
@@ -48,4 +59,46 @@ const GroupsContainer = styled.div`
   gap: 1.5rem;
 `;
 
-export default History;
+const LoadingContainer = styled.div`
+  height: 400px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function groupTransactionsByDate(transactionList: TransactionType[]) {
+  const grouped: { date: string; transactions: TransactionType[] }[] = [];
+
+  transactionList.forEach((t) => {
+    // unix time to: 'Mon 30 02 2022'
+    const dateString = format(fromUnixTime(t.date.seconds), 'iii dd LL yyyy');
+
+    let hasGroup = false;
+
+    if (!grouped.length) {
+      grouped.push({
+        date: dateString,
+        transactions: [t],
+      });
+      return;
+    }
+
+    grouped.forEach((g) => {
+      if (g.date === dateString) {
+        hasGroup = true;
+        g.transactions.push(t);
+        return;
+      }
+    });
+
+    if (!hasGroup) {
+      grouped.push({
+        date: dateString,
+        transactions: [t],
+      });
+      return;
+    }
+  });
+
+  return grouped;
+}
